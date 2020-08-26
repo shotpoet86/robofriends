1b51798f3edaa7eb24109e942ff6fd6a8ed75113
shotpoet86 <curlybracemaster@gmail.com>
2020-08-26 14:51:29 -0400

    changed function to class on home page added some css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './Hello';
 import App from './App';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/*    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,*/
+    <Hello/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
